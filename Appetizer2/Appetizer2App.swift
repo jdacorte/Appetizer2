6524d5e846300cd77adcc82f9f19902f6f3a029f
John DaCorte <john.dacorte@gmail.com>
2021-11-23 16:45:36 -0500

    Initial Commit

---
+++ Appetizer2/Appetizer2App.swift
@@ -0,0 +1,17 @@
+//
+//  Appetizer2App.swift
+//  Appetizer2
+//
+//  Created by John DaCorte on 11/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Appetizer2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
